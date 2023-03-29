516886baedf2cb8b5840cdead3867a932b9cb390
jdmpictures <74938827+jdmpictures@users.noreply.github.com>
2023-03-29 22:31:04 +0100

    added click events and clear funtion

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+//Initalising variables 
+let buttons = document.getElementsByTagName('span');
+let inputScreen = document.getElementById('input-screen')
+
+//Sets the time in the top left of the screen
 const refreshTime = () => {
 var time = new Date().toLocaleTimeString()
 document.getElementById("time").textContent = time;
 }
 setInterval(refreshTime, 1000);
 
-const buttons = document.getElementsByTagName("span")
 
+//Gets values of buttons and calls revelant function
+window.onload = function()  {
+for(let i = 0; i < buttons.length; i++) {
+    if (buttons[i].innerHTML === 'AC') {
+        buttons[i].addEventListener("click", clear)    
+    } 
+}
+}
 
-for(i = 0; i < buttons.length; i++) {
-    if(buttons[i].innerHTML === '=') {
-        buttons[i].addEventListener("click", console.log('boom'))
-    }
+const clear = () => {
+    inputScreen.textContent = "0"
+
 }
