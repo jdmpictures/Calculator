6594ba11cdb2a07f15f79f36705eac0600632494
jdmpictures <74938827+jdmpictures@users.noreply.github.com>
2023-04-20 22:38:58 +0100

    Small JS changes

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 let buttons = [...document.getElementsByTagName('span')];
 let inputScreen = document.getElementById('input-screen');
 let operator =  false;
-let operatorValue;
-let secondVal = '0'; 
-let operation = []; 
+let currentOpertor;
+let secondVal = 0; 
+
 
 
 //Sets the time in the top left of the screen
@@ -28,14 +28,20 @@ buttons.forEach(element => {
         if (element.innerHTML === '%'){
             percent();
         }
-        if (element.innerHTML === '%'){
-            percent();
-        }
         if (element.innerHTML === 'c'){
             cancel();
         }
         if (element.innerHTML === '÷'){
             divide(element);
+        }
+        if (element.innerHTML === 'x'){
+            mutiply(element);
+        }
+        if (element.innerHTML === '-'){
+            minus(element);
+        }
+        if (element.innerHTML === '+'){
+            sum(element);
         }
         else {
             inputNums(element)
@@ -46,28 +52,29 @@ buttons.forEach(element => {
     
 });
 
-// function button()  {
-// for(let i = 0; i < buttons.length; i++) {
-//     if (buttons[i].innerHTML === 'AC') {
-//         buttons[i].addEventListener("click", clear)    
-//     } 
-//     if (buttons[i].innerHTML === '+/-') {
-//         buttons[i].addEventListener("click", invert)    
-//     } 
-//     if (buttons[i].innerHTML === '%') { 
-//         buttons[i].addEventListener("click", perecent)  
-//     }
-//     if (buttons[i].innerHTML === 'c') {
-//         buttons[i].addEventListener("click", cancel)
-//     }  
-//     else {
-//         buttons[i].addEventListener("click", operate(i))
-            
-//     }
-//     }
-  
-// }
 
+const operation = (element) => {
+    if (inputScreen.innerHTML === '0' && secondVal === '0') {
+        return;
+    }
+    if(currentOpertor !== element.innerHTML){
+        switch(currentOpertor) { 
+            case '÷':
+                console.log('works')
+                break;
+            case 'x':
+                console.log('works')
+                break;
+            case '-':
+                console.log('works')
+                break;
+            case '+':
+                console.log('works')
+                break;
+        }
+    }
+
+}
 
 
 //Calculator functions 
@@ -75,21 +82,19 @@ buttons.forEach(element => {
 
 const inputNums = (element)  => {
 
-    if (element.className === 'grey' || element.className === 'operator' || (element.innerHTML === '.' && inputScreen.innerHTML.includes('.')  )){
+    if (element.className === 'grey' || element.className === 'operator' || (element.innerHTML === '.' && inputScreen.innerHTML.includes('.'))){
         return;
     }
-    if (inputScreen.innerHTML === '0' && operator === false){
+    else if (inputScreen.innerHTML === '0' || operator === true ){
+        operator = false;
         inputScreen.innerHTML = '';
-        inputScreen.innerHTML += element.innerHTML;
+        inputScreen.innerHTML += element.innerHTML; 
      } 
     else {
         inputScreen.innerHTML += element.innerHTML;
     }
 
 }
-    
-    
-
 
 const clear = () => {
     inputScreen.textContent = '0';
@@ -100,49 +105,94 @@ const clear = () => {
 const cancel = () => {
 
 }
+
+
 const invert = () => {
     let input = inputScreen.textContent;
     if (input > 0 ) {
         inputScreen.textContent = "-" + input;   
     } 
-    if (input.includes('-')){
+    else if (input.includes('-')){
         inputScreen.textContent = input.substring(1)
     }
 
 }
 
 const percent = () => {
-    inputScreen.textContent /= 10;
+    inputScreen.textContent /= 100;
 }
 
 const divide = (element) => {
     if (inputScreen.innerHTML === '0' && secondVal === '0') {
+        alert('MORON')
         return;
     } else if (inputScreen.innerHTML !== '0' && secondVal === '0') {
+        operator = true;
         secondVal = inputScreen.innerHTML;
-        inputScreen.innerHTML = '0';
-        operatorValue += element.innerHTML;
+        // operatorValue += element.innerHTML;
 
     } else {
         inputScreen.innerHTML = secondVal / inputScreen.innerHTML;
+        operator = true;7
+    }        
     }
 
 
+
+
+
+
+
+const mutiply = (element) => {
+    if (inputScreen.innerHTML === '0' && secondVal === '0') {
+        return;
+    } else if (inputScreen.innerHTML !== '0' && secondVal === '0') {
+        operator = true;
+        secondVal = inputScreen.innerHTML;
+        operatorValue += element.innerHTML;
+
+    } else {
+        inputScreen.innerHTML = secondVal * inputScreen.innerHTML;
+        operator = true;
+    }
+
 }
 
-const sum = (a, b) => {
-    return a + b; 
+const minus = (element) => {
+    if (inputScreen.innerHTML === '0' && secondVal === '0') {
+        return;
+    } else if (inputScreen.innerHTML !== '0' && secondVal === '0') {
+        operator = true;
+        secondVal = inputScreen.innerHTML;
+        operatorValue += element.innerHTML;
+
+    } else {
+        inputScreen.innerHTML = secondVal - inputScreen.innerHTML;
+        operator = true;
+    }
+
+}
+
+const sum = (element) => {
+    if (inputScreen.innerHTML === '0' && secondVal === '0') {
+        return;
+    } else if (inputScreen.innerHTML !== '0' && secondVal === '0') {
+        operator = true;
+        secondVal = inputScreen.innerHTML;
+        operatorValue += element.innerHTML;
+
+    } else {
+        inputScreen.innerHTML += secondVal;
+        operator = true;
+    }
 
 }
 
 
-const mutiply = () => {
 
-}
 
-const minus = () => {
 
-}
+
 
 const equals = () => {
 
