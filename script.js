8f6e7269de7628f4d8c621ec7e0125075b751b3c
jdmpictures <74938827+jdmpictures@users.noreply.github.com>
2023-05-15 12:35:49 +0100

    hightlight button and operator functions added

--- script.js
+++ script.js
@@ -15,10 +15,11 @@ document.getElementById("time").textContent = time;
 }
 setInterval(refreshTime, 1000);
 
-//Add the click events to the buttons 
+//Add the click events to the buttons and runs relevant functions
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
       const buttonText = button.textContent;
+      
       switch (buttonText) {
         case "AC":
           clear();
@@ -38,16 +39,51 @@ buttons.forEach((button) => {
         case "+":
         case "=":
           operation(button);
+          
           break;
         default:
           inputNums(button);
           break;
       }
+  
+      if (button.className !== 'operator') {
+        highlightButton(button);
+      } else {
+
+
+      }
+      
+      
+
     });
   });
 
+  const highlightButton = (button) => {
+    button.style.backgroundColor = 'white'; // set highlight color
+    button.style.transition = 'background-color 0.2s ease'; // set transition duration and easing
+    setTimeout(() => {
+      button.style.backgroundColor = ''; // reset to original color after 0.2s
+    }, 200);
+  }
+
+
+
+
+const highlightOperator  = (btn) => {
+    if(btn.textContent !== currentOpertor && btn.className === 'operator') {
+      btn.style.backgroundColor = '#FF9500'
+      btn.style.color = 'white'
+    } if(btn.textContent === currentOpertor && btn.className === 'operator') {
+      btn.style.backgroundColor = 'white'
+      btn.style.color = '#FF9500' 
+    }
+  }
+  
+
+  
+
+
 const calculate = (element) => {
-    console.log(currentOpertor, inputScreen.textContent, secondVal)
     newNum = true;
     switch(element) {
         case '÷':
@@ -63,16 +99,18 @@ const calculate = (element) => {
             result = Number(secondVal) + Number(inputScreen.textContent);
             break;
         case '=':
-            console
             calculate(currentOpertor);
+            currentOpertor = '';
             break;
     }
+    
     inputScreen.textContent = result;
     secondVal = result;
 
 }
 
 const operation =  (element) => {
+  highlightOperator(element)
     if (inputScreen.textContent == '0' && secondVal == 0){
         return;
     }
@@ -82,7 +120,7 @@ const operation =  (element) => {
                 newNum = true;  
     } else  {
         newNum = true;
-        if (element.textContent !== currentOpertor){
+      if (element.textContent !== currentOpertor && element.textContent !== '=' && newNum){
             calculate(currentOpertor);
             currentOpertor = element.textContent; 
         } else 
@@ -93,20 +131,18 @@ const operation =  (element) => {
 
 //Calculator functions 
 
-
-const inputNums = (e)  => {
-    if (e.className === 'grey' || e.className === 'operator' || (e.textContent === '.' && inputScreen.textContent.includes('.'))){
-        return;
-    } if (inputScreen.textContent == 0 || newNum === true){
-        inputScreen.textContent = ''; 
-        inputScreen.textContent += e.textContent;
-        deleteButton.innerHTML = '&#8592;'
-        newNum = false;
-    } else {
-        inputScreen.textContent += e.textContent;
-        deleteButton.innerHTML = '&#8592;'
+const inputNums = (e) => {
+    const buttonText = e.textContent.trim();
+    const invalidInputPattern = /^(\.|\d.*\.)$/;
+    
+    if (invalidInputPattern.test(buttonText)) {
+      return;
     }
-}
+  
+    inputScreen.textContent = (inputScreen.textContent === '0' || newNum) ? buttonText : inputScreen.textContent + buttonText;
+    deleteButton.innerHTML = '&#8592;'
+    newNum = false;
+  };
 
 const clear = () => {
     inputScreen.textContent = '0';
@@ -115,39 +151,40 @@ const clear = () => {
     currentOpertor = '';
     newNum = true;
     secondValScreen.textContent = ''
-}
+};
 
 const cancel = () => {
     let currentValue = inputScreen.textContent
-    if(currentValue.length < 1) { 
+    if(currentValue.length > 1) { 
         inputScreen.textContent = currentValue.slice(0, -1);
     } else {
         inputScreen.textContent = 0;
         deleteButton.textContent = 'AC'
     }  
-}
+};
 
 const percent = () => {
     inputScreen.textContent /= 100;
-}
+};
 
 
 const invert = () => {
-    let input = inputScreen.textContent;
-    if (input > 0 ) {
+    let input = inputScreen.textContent; // Sets current input screen value to 
+    if (input > 0 ) { 
         inputScreen.textContent = "-" + input;   
     } else if (input.includes('-')){
         inputScreen.textContent = input.substring(1)
     }
 
-}
+};
 
-const setButtonColour = (button) => {
-    const activeOperator = [...document.getElementsByClassName('operator')];
-    activeOperator.forEach(e => {
-        if(e[i].textContent === button.textContent) {
-            e[i].sty
-        }
-        
-    });
-}
+
+
+// const setButtonColour = (element) => {
+//   console.log(element.textContent)
+//   // console.log(button)
+
+//   };
+
+
+
